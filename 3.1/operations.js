b7ea838eeff187b0b637e676dac4c8ad5467b3ad
Wesley Oliveira <cont.wesleyoliv67@gmail.com>
2023-09-21 00:41:51 -0300

    Aula-3.1

---
+++ 3.1/operations.js
@@ -0,0 +1,11 @@
+let salary = 3501;
+let bonus = 2000;
+let vacations = 1000;
+let taxes = 782;
+
+console.log(salary * 12);
+console.log(salary * 12 + bonus + vacations - taxes);
+console.log(salary / 4);
+console.log(salary % 2);
+console.log(salary += 10);
+console.log(salary -= 1);
